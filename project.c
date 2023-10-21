474ea6b1bfc900d3d5253f74acc7136e46a6b465
Atharv <atharv5873@gmail.com>
2023-10-21 22:07:17 +0530

    Added struct `check` to store the employee number to check for duplicates, added function `check_dup` to check for duplicate employee numbers, added code to `new_emp` to check for duplicate employee numbers and prompt the user to enter a new employee number if a duplicate is found.

--- project.c
+++ project.c
@@ -1,6 +1,87 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+
+int main_exit;
+
+struct{
+    int emp_no;
+    char name[60];
+    int basic;
+    int da;
+    int hra;
+}add,upd,check;
+
+void close()
+{
+    printf("\n\n\n\nThis C Project is developed by Atharv Sharma!");
+}
+
+int check_dup(int empno)
+{
+    int test=0;
+    FILE *ptr;
+    ptr=fopen("record.dat","r");
+    while(fscanf(ptr,"%d %s %d %d %d\n",&add.emp_no,add.name,&add.basic,&add.da,&add.hra)!=EOF)
+    {
+        if(empno==add.emp_no)
+        {
+            test=1;
+            break;
+        }
+        fclose(ptr);
+        return test;
+    }
+}
+void new_emp()
+{
+    int choice,empno,i=0,t,ch=0,x;
+    FILE *ptr,*ptr1;
+    ptr1=fopen("newrec.dat","a+");
+    emp_no:
+    system("cls");
+    printf("\t\t\t ADD RECORD");
+    printf("\nEnter the Employee Number : ");
+    scanf("%d",&check.emp_no);
+    t=check_dup(check.emp_no);//function not written
+    ptr=fopen("record.dat","a+");
+    while (fscanf(ptr,"%d%d%d%d\n",&add.emp_no,add.name,&add.basic,&add.da,&add.hra)==1)
+    {   
+        if(t==1)
+        printf("Employee already exists");
+    }
+    if(i!=1)
+    {
+        add.emp_no=check.emp_no;
+        printf("Enter Name:");
+        scanf("%s",add.name);
+        printf("\nEnter the  Basic Salary: ");
+        scanf("%d",&add.basic);
+        printf("\nEnter the  Dearness Allowance: ");
+        scanf("%d",&add.da);
+        printf("\nEnter the Home Rent Allowance: ");
+        scanf("%d",&add.hra);
+        fssek(ptr,0,SEEK_END);
+        fprintf(ptr,"%d%d%d%d\n",&add.emp_no,add.name,&add.basic,&add.da,&add.hra);
+        fseek(ptr,0,SEEK_SET);
+        fflush(ptr);
+        printf("\nEmployee Creted Successfully!");
+    }
+    add_invalid:
+    printf("\n\n\n\t\tEnter 1 to go to the main menu and 0 to exit");
+    scanf("%d",main_exit);//not defined rn
+    system("cls");
+    if(main_exit==1)
+    menu();
+    else if(main_exit==0)
+    close();
+    else
+    {
+        printf("\nInvalid Entry! Try Again.");
+        goto add_invalid;
+    }
+}
+    
 void menu()
 {
     system("cls");
@@ -9,6 +90,7 @@ void menu()
     printf("\n\n\t\tWELCOME TO THE MAIN MENU");
     printf("\n\n\t\t1.Create new Employee\n\t\t2.Edit Employee\n\t\t3.Enter Monthly Transaction\n\t\t4.Process Payroll\n\t\t5.Reports\n\t\t6.EXIT");
     printf("\n\n\n\n\n\t\t Enter yout Choice: ");
+    scanf("%d",&choice);
 
     system("cls");
     switch (choice)
